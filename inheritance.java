ee3ce1926f43596b89006db630c42b0bf676224f
russgards03 <99627318+russgards03@users.noreply.github.com>
2022-12-03 20:37:57 +0800

    Update inheritance.java

--- inheritance.java
+++ inheritance.java
@@ -4,10 +4,10 @@ public class ValuesResult {
     public static int computeValue(String value1, String value2){
         int val1 = Integer.parseInt(value1);
         int val2 = Integer.parseInt(value2);
-        int sum = val1 + val2;
-        return sum;
+        int ans = val1 * val2;
+        return ans;
     }
     public static void displayMessage(String message){
-        JOptionPane.showMessageDialog( null, message, "Result" , JOptionPane.INFORMATION_MESSAGE );
+        JOptionPane.showMessageDialog( null, message, "The answer is!: " , JOptionPane.INFORMATION_MESSAGE );
     }
 }
